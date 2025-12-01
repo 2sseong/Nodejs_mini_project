217a67a845257c09feb3a3518e7a62dd626ac71b
2sseong <sh999051@naver.com>
2025-12-01 14:33:22 +0900

    css 변경

--- client/src/pages/MyInfoPage.jsx
+++ client/src/pages/MyInfoPage.jsx
@@ -1,8 +1,7 @@
 export default function NotificationsPage() {
     return (
         <div style={{ padding: 24 }}>
-            <h2>알림</h2>
-            <p>알림 리스트를 여기에 표시.</p>
+            <h2>내 정보 페이지임</h2>
         </div>
     )
 }
